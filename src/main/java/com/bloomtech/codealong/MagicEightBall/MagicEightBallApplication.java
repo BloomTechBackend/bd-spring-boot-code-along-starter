ebda5925f24d5183c0e90cdf96fda8f5f47ca71f
Frank Fella <frank.fella@lambdaschool.com>
2022-08-08 13:16:56 -0700

    FFella Push 08/08/2022 13:16:56

---
+++ src/main/java/com/bloomtech/codealong/MagicEightBall/MagicEightBallApplication.java
@@ -0,0 +1,13 @@
+package com.bloomtech.codealong.MagicEightBall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicEightBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicEightBallApplication.class, args);
+	}
+
+}
